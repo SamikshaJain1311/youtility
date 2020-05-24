fbdf45e3bf8c00fdfb087103c3cb16a5f132ee16
SamikshaJain1311 <jainsamiksha89@gmail.com>
2020-05-24 08:21:14 +0100

    added project files

---
+++ src/test/java/org/example/Test1.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Test1 {
+    @Given("^I open the youtility app$")
+    public void i_open_the_youtility_app() throws Throwable {
+
+    }
+
+    @When("^I click on Get Started button$")
+    public void i_click_on_Get_Started_button() throws Throwable {
+
+    }
+
+    @Then("^I should be at Terms & Privicy Policy screen$")
+    public void i_should_be_at_Terms_Privicy_Policy_screen() throws Throwable {
+
+    }
+
+    @Given("^I am at Terms and Privacy Policy screen$")
+    public void i_am_at_Terms_and_Privacy_Policy_screen() throws Throwable {
+
+    }
+
+    @When("^I check the box to accept Terms of service$")
+    public void i_check_the_box_to_accept_Terms_of_service() throws Throwable {
+
+    }
+
+    @When("^I click the continue button$")
+    public void i_click_the_continue_button() throws Throwable {
+
+    }
+
+    @Then("^I navigate to next screen to enter the email address$")
+    public void i_navigate_to_next_screen_to_enter_the_email_address() throws Throwable {
+
+    }
+
+    @Given("^I am at the email address page$")
+    public void i_am_at_the_email_address_page() throws Throwable {
+
+    }
+
+    @When("^I fill the email address textbox with value 'myname@mymail\\.com'$")
+    public void i_fill_the_email_address_textbox_with_value_myname_mymail_com() throws Throwable {
+
+    }
+
+    @Then("^a text 'Thank You' should appear on the screen$")
+    public void a_text_Thank_You_should_appear_on_the_screen() throws Throwable {
+
+    }
+}
